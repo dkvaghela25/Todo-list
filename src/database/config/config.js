5a7bac282879be56d06653b0ec7adbe23565716c
Divyang Vaghela <dkvaghela724@gmail.com>
2025-03-27 19:14:31 +0530

    Docker complete

--- src/database/config/config.js
+++ src/database/config/config.js
@@ -5,10 +5,19 @@ const development = {
   username: process.env.DATABASE_USERNAME,
   password: process.env.DATABASE_PASSWORD,
   database: process.env.DATABASE_NAME,
-  host: process.env.DATABASE_HOST,
+  host: process.env.DATABASE_HOST || 'host.docker.internal',
   port: process.env.DATABASE_PORT,
   dialect: "postgres",
 };
+
+// const development = {
+//   username: 'postgres',
+//   password: 'Vaghela@1999',
+//   database: 'To-Do List',
+//   host: 'host.docker.internal',
+//   port: 5432,
+//   dialect: "postgres",
+// };
 
 const production = {
   username: process.env.DATABASE_USERNAME,
